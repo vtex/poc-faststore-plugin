93b128841d3c2349a3a93501d53729f9d9b31f62
Guilherme Carvalho <77246+gvc@users.noreply.github.com>
2024-10-28 18:09:30 -0300

    Basic plugin attempt

---
+++ src/pages/my-account.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MyAccountSection from '../sections/MyAccount'
+
+export const path = '/my-account'
+
+export default function MyAccount() {
+  return <>
+    <h1>My Account page</h1>
+    <MyAccountSection />
+  </>
+}
---
+++ src/sections/MyAccount.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyAccountSection() {
+  return (
+    <section>
+      <h2>This is a section</h2>
+    </section>
+  )
+}
